1434d2f17ed7377a8654bc3271464b2f334f324f
LG <lmeg00001@gmail.com>
2023-02-03 11:13:24 -0500

    Changed power for output
    output function should now more appropriately output the power of the coefficient.

--- CS384_HW2/HornersMethod/hornersMethod/HornersMethod.java
+++ CS384_HW2/HornersMethod/hornersMethod/HornersMethod.java
@@ -67,7 +67,7 @@ public class HornersMethod {
 		
 		for(int i = 0; i < Coeff_List.size(); i++)
 		{
-			System.out.println("" + Coeff_List.get(i) + "x^" + i);
+			System.out.println("" + Coeff_List.get(i) + "x^" + (Coeff_List.size() - i));
 		}
 		
 		
