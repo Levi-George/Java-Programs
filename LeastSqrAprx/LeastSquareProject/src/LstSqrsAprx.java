40f35f62c573d627a28ef4f0930504c51f723fae
LG <lmeg00001@gmail.com>
2023-04-27 02:01:56 -0400

    Add libraries used
    Added libraries that I am using

---
+++ LeastSqrAprx/LeastSquareProject/src/LstSqrsAprx.java
@@ -0,0 +1,9 @@
+
+import java.io.File;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.math.*;
